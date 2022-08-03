747c8fd27e53ccfeec2c39afe17a2578c7dc3996
no1rain <no1rain@gmail.com>
2022-08-03 12:51:18 +0900

    first commit

--- test_code1.js
+++ test_code1.js
@@ -1,4 +1,20 @@
-// 변수 「myName」에 「'제이펍'」을 대입
-let myName = '제이펍';
-// myName의 값을 alert()으로 출력하기
-alert(myName);
+/** 전화번호 입력란  */
+const phoneNumberText = document.querySelector('#phoneNumberText');
+
+/** 경고 메시지  */
+const warningMessage = document.querySelector('#warningMessage');
+
+// 문자가 입력될 때마다 내용 체크
+phoneNumberText.addEventListener('keyup', () => {
+  // 입력된 전화번호
+  const phoneNumber = phoneNumberText.value;
+  // 전화번호에 하이픈(-)이 포함되면 공백(‘ ‘)으로 변환
+  const trimmedPhoneNumber = phoneNumber.replace(/-/g, ''); // 01012345678
+
+  // 0으로 시작하는 10자리 혹은 11자리의 번호 형식 체크
+  if (/^[0][0-9]{9,10}$/.test(trimmedPhoneNumber) === false) {
+    warningMessage.innerText = '전화번호의 형식에 맞춰 입력해 주세요.';
+  } else {
+    warningMessage.innerText = '';
+  }
+});
